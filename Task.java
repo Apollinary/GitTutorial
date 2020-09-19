dec4888b4dbc9a6ad51e3389aa168120114e1e44
Apollinary <apollinariiuvarov@mail.ru>
2020-09-19 09:59:44 +0300

    First commit in new_branch

--- Task.java
+++ Task.java
@@ -3,7 +3,8 @@ package at;
 public class Task {
 
 	public static void main(String args[]) {
-		
+		System.out.println("This line added into new_branch.");
+
 		int firstNumber = Integer.valueOf(args[0]);
 		int secondNumber = Integer.valueOf(args[1]);
 		
